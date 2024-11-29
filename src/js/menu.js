da9243796eeedf3e75603bc435dd4eb0ebd12ac3
Fabián Huertas <miloh9318@hotmail.com>
2024-11-28 21:42:08 -0500

    creando la clase menu-aside y creando las primeras variables en el archivo js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    
+
+    let button = document.querySelector(".menu-toggle");
+    let aside = document.querySelector(".menu-aside");
 })
